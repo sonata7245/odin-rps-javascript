5287d5308efccc4933e6f9119664efcf686a7ec2
sonata7245 <38821732+sonata7245@users.noreply.github.com>
2022-09-22 10:28:23 -0700

    Add buttons, test click listener

---
+++ app.js
@@ -0,0 +1,123 @@
+let userChoice;
+let computerChoice;
+let winner;
+let playAgain;
+//get input from user
+
+const buttonClicked = document.querySelectorAll('button');
+
+console.log (dir.buttonClicked)
+
+
+
+ //   getChoice();
+    console.log(userChoice);
+    //have computer randomly choose rock paper scissors
+ //   getComputerChoice();
+    console.log(computerChoice);
+ //   getResults()
+    console.log(winner);
+
+    //output who won
+    if (winner == "user") {
+        alert(`You chose ${userChoice} and the computer chose ${computerChoice}, You won!`)
+    }
+
+    if (winner == "computer") {
+        alert(`You chose ${userChoice} and the computer chose ${computerChoice},You lost, sorry!`)
+    }
+
+    if (winner == "tie") {
+        alert(`You chose ${userChoice} and the computer chose ${computerChoice}, What fate, its a tie!`)
+    }
+
+
+//function to get input from user
+function getChoice() {
+    let keepGoing = true;
+    while (keepGoing) {
+        userChoice = prompt("Input Rock, Paper, or Scissors");
+        //make input all uppercase
+        userChoice = userChoice.toUpperCase();
+
+
+        //make sure input is either Rock Paper or Scissors
+        if ((userChoice !== "ROCK") && (userChoice !== "PAPER") && (userChoice !== "SCISSORS")) {
+            alert("Your choice is invalid please try again");
+
+
+        }
+
+        else {
+            keepGoing = false;
+        }
+    }
+    return userChoice;
+}
+
+
+
+
+
+//Computer makes choice
+function getComputerChoice() {
+    let choiceArray = ["ROCK", "PAPER", "SCISSORS"]
+    let compRandom = getRandomInt(3);
+    computerChoice = choiceArray[compRandom];
+    return computerChoice;
+}
+
+//random number generator
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+
+
+//compare user input to computer
+//Rock > Scissor > Paper, Paper > Rock
+function getResults() {
+    if (userChoice == "ROCK") {
+
+        if (computerChoice == "SCISSORS") {
+            winner = "user";
+        }
+
+        else if (computerChoice == "PAPER") {
+            winner = "computer";
+        }
+        else if (computerChoice == "ROCK") {
+            winner = "tie";
+        }
+    }
+
+    if (userChoice == "PAPER") {
+
+        if (computerChoice == "ROCK") {
+            winner = "user";
+        }
+
+        else if (computerChoice == "SCISSORS") {
+            winner = "computer";
+        }
+        else if (computerChoice == "PAPER") {
+            winner = "tie";
+        }
+    }
+
+    if (userChoice == "SCISSORS") {
+
+        if (computerChoice == "PAPER") {
+            winner = "user";
+        }
+
+        else if (computerChoice == "ROCK") {
+            winner = "computer";
+        }
+        else if (computerChoice == "SCISSORS") {
+            winner = "tie";
+        }
+    }
+    return winner;
+}
+
