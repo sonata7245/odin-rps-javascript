f9f1824d67b82f26320c99841c64797d303ce62e
sonata7245 <38821732+sonata7245@users.noreply.github.com>
2022-09-22 11:58:01 -0700

    try this

--- app.js
+++ app.js
@@ -4,136 +4,118 @@
 //let playAgain;
 //get input from user
 
-function setUserChoice (button) {
-    console.log(button);
+function setUserChoice(choice) {
+    return userChoice = choice;
 }
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+
+function displayWinner() {
+    //output who won
+    if (winner == "user") {
+        console.log(`You chose ${userChoice} and the computer chose ${computerChoice}, You won!`)
+    }
+    if (winner == "computer") {
+        console.log(`You chose ${userChoice} and the computer chose ${computerChoice},You lost, sorry!`)
+    }
+
+    if (winner == "tie") {
+        console.log(`You chose ${userChoice} and the computer chose ${computerChoice}, What fate, its a tie!`)
+    }
+}
+
+function getComputerChoice() {
+    let choiceArray = ["ROCK", "PAPER", "SCISSORS"]
+    let compRandom = getRandomInt(3);
+    computerChoice = choiceArray[compRandom];
+    return computerChoice;
+}
+
+//compare user input to computer
+//Rock > Scissor > Paper, Paper > Rock
+function getResults() {
+    if (userChoice == "ROCK") {
+
+        if (computerChoice == "SCISSORS") {
+            winner = "user";
+        }
+
+        else if (computerChoice == "PAPER") {
+            winner = "computer";
+        }
+        else if (computerChoice == "ROCK") {
+            winner = "tie";
+        }
+    }
+
+    if (userChoice == "PAPER") {
+
+        if (computerChoice == "ROCK") {
+            winner = "user";
+        }
+
+        else if (computerChoice == "SCISSORS") {
+            winner = "computer";
+        }
+        else if (computerChoice == "PAPER") {
+            winner = "tie";
+        }
+    }
+
+    if (userChoice == "SCISSORS") {
+
+        if (computerChoice == "PAPER") {
+            winner = "user";
+        }
+
+        else if (computerChoice == "ROCK") {
+            winner = "computer";
+        }
+        else if (computerChoice == "SCISSORS") {
+            winner = "tie";
+        }
+    }
+    return winner;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 const rock = document.querySelector('#rock')
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
 
 rock.addEventListener('click', () => {
-    setUserChoice('rock');
+    setUserChoice('ROCK');
+    getComputerChoice();
+    getResults();
 })
 paper.addEventListener('click', () => {
-    setUserChoice('paper');
+    setUserChoice('PAPER');
 })
 scissors.addEventListener('click', () => {
-    setUserChoice('scissors');
+    setUserChoice('SCISSORS');
 })
 
 
 
 
 
- //   getChoice();
- //   console.log(userChoice);
-    //have computer randomly choose rock paper scissors
- //   getComputerChoice();
- //   console.log(computerChoice);
- //   getResults()
- //   console.log(winner);
-
-//     //output who won
-//     if (winner == "user") {
-//         alert(`You chose ${userChoice} and the computer chose ${computerChoice}, You won!`)
-//     }
-
-//     if (winner == "computer") {
-//         alert(`You chose ${userChoice} and the computer chose ${computerChoice},You lost, sorry!`)
-//     }
-
-//     if (winner == "tie") {
-//         alert(`You chose ${userChoice} and the computer chose ${computerChoice}, What fate, its a tie!`)
-//     }
-
-
-// //function to get input from user
-// function getChoice() {
-//     let keepGoing = true;
-//     while (keepGoing) {
-//         userChoice = prompt("Input Rock, Paper, or Scissors");
-//         //make input all uppercase
-//         userChoice = userChoice.toUpperCase();
-
-
-//         //make sure input is either Rock Paper or Scissors
-//         if ((userChoice !== "ROCK") && (userChoice !== "PAPER") && (userChoice !== "SCISSORS")) {
-//             alert("Your choice is invalid please try again");
-
-
-//         }
-
-//         else {
-//             keepGoing = false;
-//         }
-//     }
-//     return userChoice;
-// }
 
 
 
 
 
-// //Computer makes choice
-// function getComputerChoice() {
-//     let choiceArray = ["ROCK", "PAPER", "SCISSORS"]
-//     let compRandom = getRandomInt(3);
-//     computerChoice = choiceArray[compRandom];
-//     return computerChoice;
-// }
-
-// //random number generator
-// function getRandomInt(max) {
-//     return Math.floor(Math.random() * max);
-// }
-
-
-
-// //compare user input to computer
-// //Rock > Scissor > Paper, Paper > Rock
-// function getResults() {
-//     if (userChoice == "ROCK") {
-
-//         if (computerChoice == "SCISSORS") {
-//             winner = "user";
-//         }
-
-//         else if (computerChoice == "PAPER") {
-//             winner = "computer";
-//         }
-//         else if (computerChoice == "ROCK") {
-//             winner = "tie";
-//         }
-//     }
-
-//     if (userChoice == "PAPER") {
-
-//         if (computerChoice == "ROCK") {
-//             winner = "user";
-//         }
-
-//         else if (computerChoice == "SCISSORS") {
-//             winner = "computer";
-//         }
-//         else if (computerChoice == "PAPER") {
-//             winner = "tie";
-//         }
-//     }
-
-//     if (userChoice == "SCISSORS") {
-
-//         if (computerChoice == "PAPER") {
-//             winner = "user";
-//         }
-
-//         else if (computerChoice == "ROCK") {
-//             winner = "computer";
-//         }
-//         else if (computerChoice == "SCISSORS") {
-//             winner = "tie";
-//         }
-//     }
-//     return winner;
-// }
 
